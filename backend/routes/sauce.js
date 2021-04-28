1e4410abadc2a309bbfb320d64f412627fe719de
hanan <hananzarrou@gmail.com>
2021-04-28 17:14:15 +0200

    add new route

--- backend/routes/sauce.js
+++ backend/routes/sauce.js
@@ -6,10 +6,11 @@ const multer = require('../middleware/multer-config');
 
 const sauceCtrl = require('../controllers/sauce');
 
-router.get('/', auth, sauceCtrl.getAllsauce);
-router.post('/', auth, multer, sauceCtrl.createThing);
-router.get('/:id', auth, sauceCtrl.getOneThing);
-router.put('/:id', auth, multer, sauceCtrl.modifyThing);
-router.delete('/:id', auth, sauceCtrl.deleteThing);
+router.get('/', auth, sauceCtrl.getAllSauce);
+router.post('/', auth, multer, sauceCtrl.createSauce);
+router.get('/:id', auth, sauceCtrl.getOneSauce);
+router.put('/:id', auth, multer, sauceCtrl.modifySauce);
+router.delete('/:id', auth, sauceCtrl.deleteSauce);
+router.post('/:id/like', auth, sauceCtrl.addLike);
 
 module.exports = router;
