9d4a7f86edd4b879cec2c6cbcef8389bc0e9820e
hanan <hananzarrou@gmail.com>
2021-04-28 13:28:51 +0200

    add modif sauce

--- backend/app.js
+++ backend/app.js
@@ -1,21 +1,23 @@
 //4 eme etape creer des routes et donc remplacer tout les mildwares par ceci //
+require('dotenv').config()
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const path = require('path');
+const helmet = require("helmet");
 
-const saucesRoutes = require('./routes/sauces');
+const sauceRoutes = require('./routes/sauce');
 const userRoutes = require('./routes/user');
 
 //mongoose.connect(`mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@cluster0.5fuiw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`,//
-mongoose.connect('mongodb+srv://hananzarrou:13021974hanan@cluster0.5fuiw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+mongoose.connect('mongodb+srv://hananprojet6:13131313@cluster0.5fuiw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
   { useNewUrlParser: true,
     useUnifiedTopology: true })
   .then(() => console.log('Connexion à MongoDB réussie !'))
   .catch(() => console.log('Connexion à MongoDB échouée !'));
 
 const app = express();
-
+app.use(helmet());
 
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
@@ -27,7 +29,7 @@ app.use((req, res, next) => {
 app.use(bodyParser.json());
 
 app.use('/images', express.static(path.join(__dirname, 'images')));
-app.use('/api/sauces', saucesRoutes);
+app.use('/api/sauces', sauceRoutes);
 app.use('/api/auth', userRoutes);
 
 module.exports=app;
